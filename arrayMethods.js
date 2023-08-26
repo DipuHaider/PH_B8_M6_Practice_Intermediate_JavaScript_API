854bc25da5308c5059dd561634d2f209c8a2da7a
Dipu Haider <dipuhaider@gmail.com>
2023-08-26 15:20:16 +0600

    Update arrayMethods.js

--- arrayMethods.js
+++ arrayMethods.js
@@ -10,7 +10,7 @@
 //   console.log(arr[i]);
 // }
 
-// FOREACH########################################################
+// FOREACH ################################ Does not return anything
 //const arr = [1, 2, 3];
 
 // const a = (x) => console.log(x);
@@ -33,4 +33,38 @@
 
 // console.log(sum);
 
-// MAP#######################################################
+// MAP ################################ Returns an array
+// const arr = [1, 2, 3];
+// // const result = arr.forEach((item) => item); //Does not return anything
+// // console.log(result);//Does not return anything
+
+// const result1 = arr.map((item) => item * 2); //Returns an array
+// console.log(result1); //Returns an array
+// console.log("main array", arr); //Main array remains intact
+
+// const fruits = ["mango", "banana", "berry"];
+
+// fruits.forEach((item) => console.log(item));
+
+//es6 foreach
+// //array.forEach((element, index, mainArray)) => {})
+// fruits.forEach((item, index, arr) => console.log(item, index, arr));
+
+// FIND ################################ Returns an array
+// const fruits = ["mango", "banana", "berry"];
+// const find = (fruits) => {
+//   for (let item of fruits) {
+//     if (item === "banana") {
+//       return item;
+//     }
+//   }
+//   return null;
+// };
+
+// const x = find(fruits);
+// console.log(x);
+
+//es6 find
+const fruits = ["mango", "banana", "berry"];
+const y = fruits.find((item) => item === "banana");
+console.log(y);
