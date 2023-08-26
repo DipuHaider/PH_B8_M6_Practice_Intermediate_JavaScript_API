ae42d85727bbf028e3383d5ddfe44ee79f8ea97b
Dipu Haider <dipuhaider@gmail.com>
2023-08-26 14:37:03 +0600

    arrow function

---
+++ arrow.js
@@ -0,0 +1,29 @@
+// Conventional
+// function add(x, y) {
+//   const result = x + y;
+//   return result;
+// }
+
+// function show() {
+//   console.log("hello");
+// }
+
+// function printNumber(x) {
+//   console.log("value is", x);
+// }
+
+// console.log(add(2, 3));
+// show();
+// printNumber(5);
+
+// Arrow function
+// const add = (x, y) => x + y;
+// console.log(add(2, 5));
+
+// const show = () => {
+//   console.log("Hello");
+// };
+// show();
+
+// const printNumber = (x) => console.log("value is", x);
+// printNumber(5);
