9aab183b53880dea9c7c58cd282d4186db023895
Dipu Haider <dipuhaider@gmail.com>
2023-08-26 16:17:37 +0600

    Create ternary.js

---
+++ ternary.js
@@ -0,0 +1,9 @@
+const x = 5;
+
+// if (x > 5) {
+//   console.log("x is greater than 5");
+// } else {
+//   console.log("x is greater than 5");
+// }
+
+x > 5 ? console.log("x is greater than 5") : console.log("x is greater than 5");
