76684f04f5b805fe07407c540b27b9a3d7399315
Dipu Haider <dipuhaider@gmail.com>
2023-08-26 15:00:39 +0600

    Update arrayMethods.js

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,10 +1,36 @@
 // forEach, Map, Find, Filter, Reduce
-const arr = [1, 2, 3];
 
-for (let item of arr) {
-  console.log(item);
-}
+// const arr = [1, 2, 3];
 
-for (i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
+// for (let item of arr) {
+//   console.log(item);
+// }
+
+// for (i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// FOREACH########################################################
+//const arr = [1, 2, 3];
+
+// const a = (x) => console.log(x);
+// arr.forEach(a);
+
+//arr.forEach((x) => console.log(x));
+
+// const fruits = ["mango", "banana", "berry"];
+// fruits.forEach((item) => {
+//   console.log(item);
+// });
+
+// const arr = [1, 2, 3];
+// let sum = 0;
+// // arr.forEach((item) => {
+// //   //   sum = sum + item;
+// //   sum += item;
+// // });
+// arr.forEach((item) => (sum += item));
+
+// console.log(sum);
+
+// MAP#######################################################
