43591914d079cd9e3da509b3f931980932c5f964
Dipu Haider <dipuhaider@gmail.com>
2023-08-28 16:38:07 +0600

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,10 +1,12 @@
-const function1 = (callback) => {
+const function1 = (a, b, callback) => {
   //   console.log(typeof callback);
-  callback();
+  //const b = 10;
+  callback(a, b);
 };
 
-const callback = () => {
-  console.log("I am callback function");
+const callback = (x, y) => {
+  //   console.log("I am callback function");
+  console.log("Sum: ", x + y);
 };
 
-function1(callback);
+function1(10, 20, callback);
